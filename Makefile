69a9ccb0b7f5974e370e7026d10d46ee28475f5e
AndreasMaros <andreas.maros@gebruederheitz.de>
2021-10-27 11:02:51 +0200

    [chore] Do not track builds; add Makefile convenience

---
+++ Makefile
@@ -0,0 +1,8 @@
+build:
+	npm i && npm run build
+
+test:
+	npm i && npm run lint
+
+dev:
+	npm i && npm run build
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import { LightboxFactory } from './frontend';
-import { register, attributes, edit, save } from './editor';
-
-export { LightboxFactory, register, attributes, edit, save };
