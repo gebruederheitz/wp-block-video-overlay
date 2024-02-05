5b9d85ed58a7ad26ab698e60adf7bad0b6c31776
Andreas Gallus <andreas.maros@gebruederheitz.de>
2024-02-05 17:55:55 +0100

    fix: add empty option to not use any captions (#3)

--- src/editor/utils/getCcLangPrefOptions.js
+++ src/editor/utils/getCcLangPrefOptions.js
@@ -1,14 +1,16 @@
 /* global editorData */
 export function getCcLangPrefOptions() {
     if (editorData && editorData.ccLangPrefs) {
-        return Object.keys(editorData.ccLangPrefs).map((ccLangPref) => {
-            return {
-                label:
-                    editorData.ccLangPrefs[ccLangPref].displayName ||
-                    ccLangPref,
-                value: ccLangPref,
-            };
-        });
+        return Object.keys(editorData.ccLangPrefs)
+            .map((ccLangPref) => {
+                return {
+                    label:
+                        editorData.ccLangPrefs[ccLangPref].displayName ||
+                        ccLangPref,
+                    value: ccLangPref,
+                };
+            })
+            .unshift({ label: 'None', value: '' });
     } else {
         return null;
     }
