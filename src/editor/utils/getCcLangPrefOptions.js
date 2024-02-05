2931c53e43e1fe1f43c06058e066d210de88c428
Andreas Gallus <andreas.maros@gebruederheitz.de>
2024-02-05 18:34:55 +0100

    fix: return array of options for ccLangPref

--- src/editor/utils/getCcLangPrefOptions.js
+++ src/editor/utils/getCcLangPrefOptions.js
@@ -1,16 +1,19 @@
 /* global editorData */
 export function getCcLangPrefOptions() {
     if (editorData && editorData.ccLangPrefs) {
-        return Object.keys(editorData.ccLangPrefs)
-            .map((ccLangPref) => {
+        const options = Object.keys(editorData.ccLangPrefs).map(
+            (ccLangPref) => {
                 return {
                     label:
                         editorData.ccLangPrefs[ccLangPref].displayName ||
                         ccLangPref,
                     value: ccLangPref,
                 };
-            })
-            .unshift({ label: 'None', value: '' });
+            }
+        );
+        options.unshift({ label: 'None', value: '' });
+
+        return options;
     } else {
         return null;
     }
